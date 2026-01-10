2f1e431774cfcfe2015de11ecff7b4395dc9f27c
EPAYEMEN <yupyemeni@gmail.com>
2026-01-11 02:40:22 +0300

    Add API keys configuration in config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+// مفاتيح API الخاصة بك
+const API_KEYS = {
+    weather: "92dd1d98bf7080fffe3dcd0846a4684d",
+    air: "0343324d05e40ac39f8e7c87969111ad",
+    co2: "fe9c6c3f3490897f56d030914430fd53"
+};
