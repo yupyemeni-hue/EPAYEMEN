e6ebade59cb7e9dbe7aca8d4e24206e53e659695
EPAYEMEN <yupyemeni@gmail.com>
2026-01-11 02:41:28 +0300

    Add governorates and indicators data
    Added data for Yemeni governorates and general indicators.

---
+++ assets/js/data.js
@@ -0,0 +1,18 @@
+// بيانات المحافظات اليمنية
+const governorates = [
+    { name: "صنعاء", temp: 23.5, humidity: 64, wind: 25.8, rain: 0 },
+    { name: "عدن", temp: 29, humidity: 70, wind: 15, rain: 0 },
+    { name: "تعز", temp: 26, humidity: 68, wind: 20, rain: 0 },
+    { name: "حضرموت", temp: 32, humidity: 50, wind: 10, rain: 0 },
+    { name: "المهرة", temp: 31, humidity: 55, wind: 12, rain: 0 }
+    // أضف جميع المحافظات اليمنية الأخرى
+];
+
+// بيانات المؤشرات العامة
+const indicators = {
+    temp: 28,
+    humidity: 62,
+    wind: 18,
+    co2: 410,
+    airQuality: "جيد"
+};
