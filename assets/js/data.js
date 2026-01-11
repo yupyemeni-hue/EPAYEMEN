9830f932e231b24569481c3284a6c80b4a23695e
EPAYEMEN <yupyemeni@gmail.com>
2026-01-11 04:30:17 +0300

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,28 +1,30 @@
 // بيانات المحافظات اليمنية
 const governorates = [
-    { name: "صنعاء", temp: 23.5, humidity: 64, wind: 25.8, rain: 0 },
-    { name: "عدن", temp: 29, humidity: 70, wind: 15, rain: 0 },
-    { name: "تعز", temp: 26, humidity: 68, wind: 20, rain: 0 },
-    { name: "حضرموت", temp: 32, humidity: 50, wind: 10, rain: 0 },
-    { name: "المهرة", temp: 31, humidity: 55, wind: 12, rain: 0 }
-{ name: "الحديدة", temp: null, humidity: null, wind: null, rain: null },
-{ name: "إب", temp: null, humidity: null, wind: null, rain: null },
-{ name: "مأرب", temp: null, humidity: null, wind: null, rain: null },
-{ name: "شبوة", temp: null, humidity: null, wind: null, rain: null },
-{ name: "الجوف", temp: null, humidity: null, wind: null, rain: null },
-{ name: "ذمار", temp: null, humidity: null, wind: null, rain: null },
-{ name: "لحج", temp: null, humidity: null, wind: null, rain: null },
-{ name: "ريمة", temp: null, humidity: null, wind: null, rain: null },
-{ name: "المحويت", temp: null, humidity: null, wind: null, rain: null },
-{ name: "صعدة", temp: null, humidity: null, wind: null, rain: null },
-{ name: "عمران", temp: null, humidity: null, wind: null, rain: null },
-{ name: "أبين", temp: null, humidity: null, wind: null, rain: null },
-{ name: "الضالع", temp: null, humidity: null, wind: null, rain: null },
-{ name: "أمانة العاصمة", temp: null, humidity: null, wind: null, rain: null },
-{ name: "حضرموت الوادي", temp: null, humidity: null, wind: null, rain: null }    // أضف جميع المحافظات اليمنية الأخرى
+    { name: "صنعاء", temp: 23.5, humidity: 64, wind: 25.8, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "عدن", temp: 29, humidity: 70, wind: 15, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "تعز", temp: 26, humidity: 68, wind: 20, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "حضرموت", temp: 32, humidity: 50, wind: 10, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "المهرة", temp: 31, humidity: 55, wind: 12, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "المكلا", temp: 30, humidity: 58, wind: 14, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "ذمار", temp: 24, humidity: 63, wind: 18, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "إب", temp: 25, humidity: 65, wind: 17, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "ريمة", temp: 26, humidity: 60, wind: 16, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "صعدة", temp: 22, humidity: 67, wind: 19, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "حجة", temp: 24, humidity: 61, wind: 15, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "الحديدة", temp: 28, humidity: 72, wind: 20, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "المحويت", temp: 25, humidity: 64, wind: 16, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "لحج", temp: 29, humidity: 69, wind: 14, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "أبين", temp: 30, humidity: 66, wind: 13, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "الضالع", temp: 27, humidity: 62, wind: 15, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "البيضاء", temp: 26, humidity: 61, wind: 12, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "شبوة", temp: 31, humidity: 60, wind: 11, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "حضرموت_الوادي", temp: 33, humidity: 52, wind: 10, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "صنعاء_الريفية", temp: 23, humidity: 65, wind: 18, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "تعز_الريفية", temp: 25, humidity: 64, wind: 16, rain: 0, co2: 410, airQuality: "جيد" },
+    { name: "ذمار_الريفية", temp: 24, humidity: 63, wind: 17, rain: 0, co2: 410, airQuality: "جيد" }
 ];
 
-// بيانات المؤشرات العامة
+// مؤشرات عامة
 const indicators = {
     temp: 28,
     humidity: 62,
