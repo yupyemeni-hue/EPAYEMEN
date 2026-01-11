bb76bd62f027758c14bb230f3c46636caff74a2b
EPAYEMEN <yupyemeni@gmail.com>
2026-01-11 03:33:51 +0300

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,7 +5,21 @@ const governorates = [
     { name: "تعز", temp: 26, humidity: 68, wind: 20, rain: 0 },
     { name: "حضرموت", temp: 32, humidity: 50, wind: 10, rain: 0 },
     { name: "المهرة", temp: 31, humidity: 55, wind: 12, rain: 0 }
-    // أضف جميع المحافظات اليمنية الأخرى
+{ name: "الحديدة", temp: null, humidity: null, wind: null, rain: null },
+{ name: "إب", temp: null, humidity: null, wind: null, rain: null },
+{ name: "مأرب", temp: null, humidity: null, wind: null, rain: null },
+{ name: "شبوة", temp: null, humidity: null, wind: null, rain: null },
+{ name: "الجوف", temp: null, humidity: null, wind: null, rain: null },
+{ name: "ذمار", temp: null, humidity: null, wind: null, rain: null },
+{ name: "لحج", temp: null, humidity: null, wind: null, rain: null },
+{ name: "ريمة", temp: null, humidity: null, wind: null, rain: null },
+{ name: "المحويت", temp: null, humidity: null, wind: null, rain: null },
+{ name: "صعدة", temp: null, humidity: null, wind: null, rain: null },
+{ name: "عمران", temp: null, humidity: null, wind: null, rain: null },
+{ name: "أبين", temp: null, humidity: null, wind: null, rain: null },
+{ name: "الضالع", temp: null, humidity: null, wind: null, rain: null },
+{ name: "أمانة العاصمة", temp: null, humidity: null, wind: null, rain: null },
+{ name: "حضرموت الوادي", temp: null, humidity: null, wind: null, rain: null }    // أضف جميع المحافظات اليمنية الأخرى
 ];
 
 // بيانات المؤشرات العامة
